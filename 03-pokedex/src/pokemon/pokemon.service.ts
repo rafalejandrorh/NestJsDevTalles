bfc540b3d9537338c8e322bca58b64b76d22dabc
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-13 16:06:55 -0400

    Update Pokemon Endpoint

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -66,8 +66,15 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    
+    const pokemon = await this.findOne(term);
+    if(updatePokemonDto.name) {
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase().trim(); // Normalize the name to lowercase and trim whitespace
+    }
+
+    await pokemon.updateOne(updatePokemonDto, { new: true });
+    return { ...pokemon.toJSON(), ...updatePokemonDto };
   }
 
   remove(id: number) {
