510c3bc23affd7ecc163d56edfc7d2a28f6efd7f
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-13 12:55:24 -0400

    Set Global Prefix

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,12 +1,14 @@
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
