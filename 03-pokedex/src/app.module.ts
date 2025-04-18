41ab74d21401014e41b53538a9abce24f53704f2
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-18 15:53:24 -0400

    Environment Variables Validation Schema Implementation

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -5,15 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [EnvConfiguration]
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
