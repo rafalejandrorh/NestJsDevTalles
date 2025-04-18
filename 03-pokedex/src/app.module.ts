ccb9680d7c692011f1676730be2e3ff23106d1bf
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-18 14:17:43 -0400

    Environment Variables Implementation

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,17 +1,24 @@
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+
 import { Module } from '@nestjs/common';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/mongo'),
     PokemonModule,
     CommonModule,
     SeedModule,
@@ -19,4 +26,5 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
