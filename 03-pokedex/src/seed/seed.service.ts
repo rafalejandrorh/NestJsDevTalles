359d18e5cbcce9ad469bb16ec594e0ba8f6736be
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-18 11:20:41 -0400

    Seed Run Poke API Implementation

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+
+  constructor(private readonly PokemonService: PokemonService) {}
 
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
@@ -14,6 +17,12 @@ export class SeedService {
       const segments = url.split('/');
       const id = segments[segments.length - 2];
       console.log({ id, name });
+
+      this.PokemonService.create({
+        name,
+        no: +id
+      });
+      
     });
 
     return data.results;
