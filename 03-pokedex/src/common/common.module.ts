43e5053a7e9c9bbb8261e35a1e8c46a9c966a104
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-18 16:54:31 -0400

    Delete 03-pokedex

--- 03-pokedex/src/common/common.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AxiosAdapter } from './adapters/axios.adapters';
-
-@Module({
-    providers: [AxiosAdapter],
-    exports: [AxiosAdapter],
-})
-export class CommonModule {}
--- 03-pokedex/src/common/interfaces/http-adapter.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface HttpAdapter {
-  get<T>(url: string): Promise<T>;
-  post<T>(url: string, payload: any): Promise<T>;
-  put<T>(url: string, payload: any): Promise<T>;
-  delete<T>(url: string): Promise<T>;
-  patch<T>(url: string, payload: any): Promise<T>;
-}
