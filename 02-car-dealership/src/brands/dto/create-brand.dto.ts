8f0eb9267bce83bca7913f973fe34bf6c347c586
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-18 16:54:15 -0400

    Delete 02-Car-dealership

--- 02-car-dealership/src/brands/dto/create-brand.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-
-export class CreateBrandDto {
-
-  @IsString()
-  @MinLength(1)
-  name: string;
-
-}
--- 02-car-dealership/src/cars/interfaces/car.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Car {
-  id: string;
-  model: string;
-  brand: string;
-}
