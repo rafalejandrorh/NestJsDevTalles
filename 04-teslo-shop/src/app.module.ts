dabe2ae631a2426f8774e37a6bc52ac988f10226
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-26 20:04:04 -0400

    Install and Run PostgreSQL with Docker

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // Set to false in production
+    })
+  ],
   controllers: [],
   providers: [],
 })
