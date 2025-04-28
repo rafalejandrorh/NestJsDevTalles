518367fd496c92ad08e5ad7380921068a27b1495
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-04-28 18:48:09 -0400

    Install class-validator and class-transformer libraries

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Add the ValidationPipe to the global scope
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // This is used to transform the incoming request body to the DTO type,
+      transformOptions: {
+        enableImplicitConversion: true, // This is used to transform the incoming request body to the DTO type,
+      }
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
