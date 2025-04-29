d6b8bd7d46cb326cdd84bc971b6ebf638524db3c
Rafael Rivero <rafalejandrorivero@gmail.com>
2025-04-29 16:38:01 -0400

    OneToMany and ManyToOne Relationship betweeen Product and ProductImages Entity

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./";
 
 @Entity()
 export class Product {
@@ -48,7 +49,13 @@ export class Product {
     tags: string[];
 
     // Images
-
+    @OneToMany(
+      () => ProductImage,
+      (productImage) => productImage.product,
+      { cascade: true, eager: true }
+    )
+    images?: ProductImage;
+    
     @BeforeInsert()
     checkSlugInsert() {
       if(!this.slug) {
