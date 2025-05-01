293f956bcaaaba16470e1676433fde56e14a5328
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-01 19:20:04 -0400

    Create User Entity

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/auth.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([
+      User
+    ])
+  ],
+  exports: [
+    TypeOrmModule
+  ]
 })
 export class AuthModule {}
--- 04-teslo-shop/src/auth/entities/auth.entity.ts
+++ 04-teslo-shop/src/auth/entities/auth.entity.ts
@@ -1,1 +1,32 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    email: string;
+
+    @Column('text')
+    password: string;
+
+    @Column('text', {
+        default: 'user',
+    })
+    fullName: string;
+
+    @Column('bool', {
+        default: false,
+    })
+    isActive: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user'],
+    })
+    role: string[];
+}
