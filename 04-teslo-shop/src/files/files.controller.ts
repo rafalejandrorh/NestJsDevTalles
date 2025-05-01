1b670c7c3ae5f92608b59f68b0580dfb70735f00
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-01 17:58:57 -0400

    Save Product Images in Filesystem

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,15 +1,25 @@
-import { BadRequestException, Controller, InternalServerErrorException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from 'src/common/helpers/fileFilter';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
+
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
     fileFilter: fileFilter,
+    limits: { fileSize: 1000000 }, // 1MB
+    storage: diskStorage({
+      destination: './static/uploads/products',
+      filename: (req, file, cb) => {
+        const filename = `${Date.now()}-${file.originalname}`;
+        cb(null, filename);
+      },
+    })
   }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
       
@@ -23,7 +33,6 @@ export class FilesController {
       mimetype: file.mimetype,
     };
 
-  }
   }
 
 }
