ddd3e7a15f5782bee1b4fd7368be012fa9f4e52b
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-01 17:31:32 -0400

    Upload Product File to Backend

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,6 +1,17 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    // const { file } = req;  
+    //return this.filesService.create(createFileDto);
+    return file;
+  }
+
 }
