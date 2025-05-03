91f5ef6f78c79a891b2a72f215809d2e215ecc51
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-03 17:47:49 -0400

    isActive field is set true by default in User Entity

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/auth.entity';
+import { User } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
--- 04-teslo-shop/src/auth/entities/user.entity.ts
+++ 04-teslo-shop/src/auth/entities/user.entity.ts
@@ -20,7 +20,7 @@ export class User {
     fullName: string;
 
     @Column('bool', {
-        default: false,
+        default: true,
     })
     isActive: boolean;
 
