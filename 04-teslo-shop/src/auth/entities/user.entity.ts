9a9250de60c73b3cebe34e8f0f5055f14ffc87c7
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-06 21:49:32 -0400

    Configure ApiProperty by User

--- 04-teslo-shop/src/auth/entities/user.entity.ts
+++ 04-teslo-shop/src/auth/entities/user.entity.ts
@@ -1,53 +1,83 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Product } from "src/products/entities";
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
 export class User {
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @ApiProperty({
+    example: '02493c19-ddc4-4f10-b42d-ad2d1aa6ba5f',
+    description: 'User ID',
+    uniqueItems: true
+  })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column('text', {
-        unique: true,
-    })
-    email: string;
+  @ApiProperty({
+    example: 'test@gmail.com',
+    description: 'User Email',
+    uniqueItems: true
+  })
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
 
-    @Column('text', {
-        select: false, // This will not be selected by default when querying the user
-    })
-    password: string;
+  @ApiProperty({
+    example: 'Abc123',
+    description: 'User Password',
+  })
+  @Column('text', {
+    select: false, // This will not be selected by default when querying the user
+  })
+  password: string;
 
-    @Column('text', {
-        default: 'user',
-    })
-    fullName: string;
+  @ApiProperty({
+    default: 'user',
+    example: 'Juan Perez',
+    description: 'User Full name'
+  })
+  @Column('text', {
+    default: 'user',
+  })
+  fullName: string;
 
-    @Column('bool', {
-        default: true,
-    })
-    isActive: boolean;
+  @ApiProperty({
+    default: true,
+    example: true,
+    description: 'User Is Active',
+  })
+  @Column('bool', {
+    default: true,
+  })
+  isActive: boolean;
 
-    @Column('text', {
-        array: true,
-        default: ['user'],
-    })
-    role: string[];
+  @ApiProperty({
+    default: ['user'],
+    example: ['user'],
+    description: 'User Roles',
+  })
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  role: string[];
 
-    @OneToMany(
-        () => Product,
-        (product) => product.user
-    )
-    product: Product
+  @OneToMany(
+    () => Product,
+    (product) => product.user
+  )
+  product: Product
 
-    @BeforeInsert()
-    checkFieldsBeforeInsert() {
-        this.email = this.email.toLowerCase().trim();
-        if(this.fullName) this.fullName = this.fullName.toLowerCase().trim();
-    }
+  @BeforeInsert()
+  checkFieldsBeforeInsert() {
+    this.email = this.email.toLowerCase().trim();
+    if(this.fullName) this.fullName = this.fullName.toLowerCase().trim();
+  }
 
-    @BeforeUpdate()
-    checkFieldsBeforeUpdate() {
-        this.checkFieldsBeforeInsert();
-    }
+  @BeforeUpdate()
+  checkFieldsBeforeUpdate() {
+    this.checkFieldsBeforeInsert();
+  }
 
 }
