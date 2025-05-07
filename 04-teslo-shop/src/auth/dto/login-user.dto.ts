443a5efc225a70f5d2140465d210121fdafff7f9
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-06 21:40:21 -0400

    Configure Specs DTO by Auth

--- 04-teslo-shop/src/auth/dto/login-user.dto.ts
+++ 04-teslo-shop/src/auth/dto/login-user.dto.ts
@@ -1,11 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class LoginUserDto {
 
+  @ApiProperty({
+    description: 'User Email',
+    uniqueItems: true
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User Password',
+    minLength: 6,
+    maxLength: 50
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
