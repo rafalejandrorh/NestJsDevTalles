ee668e156a64b8b3adbeef85057aa57a0fe9e12d
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-08 17:39:04 -0400

    WebSocket Client: Multiple ways to send messages

--- 05-ws-client/src/main.ts
+++ 05-ws-client/src/main.ts
@@ -12,6 +12,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <input id="message-input" placeholder="message" />
       <button id="send-btn">Send</button>
     </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
   </div>
 `
 
--- 05-ws-client/src/socket-client.ts
+++ 05-ws-client/src/socket-client.ts
@@ -13,6 +13,7 @@ const addListeners = (socket: Socket) => {
   const clientsUl = document.querySelector('#clients-ul')!;
   const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
   const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+  const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
 
   socket.on('connect', () => {
     serverStatusLabel.innerHTML = 'connected';
@@ -46,7 +47,16 @@ const addListeners = (socket: Socket) => {
   });
 
   socket.on('message-from-server', (payload: {fullName: string, message: string}) => {
-    console.log(payload);
+    const newMessage = `
+      <li>
+        <strong>${payload.fullName}</strong>
+        <span>${payload.message}</span>
+      </li>
+    `;
+
+    const li = document.createElement('li');
+    li.innerHTML = newMessage;
+    messagesUl.append(li);
   });
 
 }
