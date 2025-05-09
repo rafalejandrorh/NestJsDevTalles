81d2c326d3f12baadded65629c0882f34d1f4a93
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-08 20:52:13 -0400

    Connect NestJs with Prisma

--- 06-report-server/src/basic-reports/basic-reports.service.ts
+++ 06-report-server/src/basic-reports/basic-reports.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateBasicReportDto } from './dto/create-basic-report.dto';
 import { UpdateBasicReportDto } from './dto/update-basic-report.dto';
+import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class BasicReportsService {
+export class BasicReportsService extends PrismaClient implements OnModuleInit {
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected to the database');
+  }
+
   create(createBasicReportDto: CreateBasicReportDto) {
     return 'This action adds a new basicReport';
   }
 
   findAll() {
-    return `This action returns all basicReports`;
+    return this.employees.findFirst();
   }
 
   findOne(id: number) {
