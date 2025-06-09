c919c77aac96c06397ff8b490624533fc86d2eb4
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-06-09 11:53:48 -0400

    Refactor: Printer Module and Reports Module

--- 06-report-server/src/basic-reports/basic-reports.service.ts
+++ 06-report-server/src/basic-reports/basic-reports.service.ts
@@ -1,20 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
-
-import PdfPrinter from 'pdfmake';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
-
-const fonts = {
-  Roboto: {
-    normal: 'fonts/Roboto-Regular.ttf',
-    bold: 'fonts/Roboto-Medium.ttf',
-    italics: 'fonts/Roboto-Italic.ttf',
-    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
-  }
-};
+import { PrinterService } from 'src/printer/printer.service';
+import { getHelloWorldReport } from 'src/reports';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
+
+  constructor(
+    private readonly printer: PrinterService
+  ) {
+    super();
+  }
 
   async onModuleInit() {
     await this.$connect();
@@ -22,13 +18,9 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
   }
 
   helloWorld() {
-    const printer = new PdfPrinter(fonts);
-    const docDefinition: TDocumentDefinitions = {
-      content: [
-        'Hola Mundo'
-      ]
-    }
-    return printer.createPdfKitDocument(docDefinition)
+    return this.printer.createPdf(getHelloWorldReport({
+      name: 'Rafael'
+    }));
   }
 
   findOne(id: number) {
---
+++ 06-report-server/src/reports/hello-world.report.ts
@@ -0,0 +1,42 @@
+import type { TDocumentDefinitions } from "pdfmake/interfaces";
+
+interface ReportOptions {
+  name: string;
+}
+
+export const getHelloWorldReport = (options: ReportOptions) => {
+  const { name } = options;
+
+  const docDefinition: TDocumentDefinitions = {
+    content: [
+      {
+        text: `Hello ${name}`,
+        style: 'header',
+      },
+      {
+        text: 'This is a simple PDF report generated using pdfmake.',
+        style: 'subheader',
+      },
+      {
+        text: 'This report demonstrates basic text formatting and styles.',
+        style: 'text',
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 24,
+        bold: true,
+        margin: [0, 0, 0, 10],
+      },
+      subheader: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 10, 0, 5],
+      },
+      text: {
+        fontSize: 12,
+      },
+    },
+  };
+  return docDefinition;
+}
