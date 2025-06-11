a8abab1fe26108641917151a051d4d6e4051a672
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-06-11 14:45:20 -0400

    Currency Formatter

---
+++ 06-report-server/src/helpers/currency-formatter.ts
@@ -0,0 +1,11 @@
+
+export class CurrencyFormatter {
+
+  static formatCurrency(value: number): string {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(value);
+  }
+
+}
